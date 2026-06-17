580c464a2666c280b77883df754d91aea718c7fe
DC24V7K533QuocThinh <thinhdc24v7k533@vlvh.ctu.edu.vn>
2026-06-16 21:28:40 -0700

    Cai dat cac handler truy xuat CSDL

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -3,15 +3,16 @@ const contacts = require("../controllers/contact.controller");
 
 const router = express.Router();
 
-router.route("/")
+router
+  .route("/")
   .get(contacts.findAll)
   .post(contacts.create)
   .delete(contacts.deleteAll);
 
-router.route("/favorite")
-  .get(contacts.findAllFavorite);
+router.route("/favorite").get(contacts.findAllFavorite);
 
-router.route("/:id")
+router
+  .route("/:id")
   .get(contacts.findOne)
   .put(contacts.update)
   .delete(contacts.delete);
